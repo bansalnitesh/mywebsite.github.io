157fe2d5fb5cf4cb095729474cb6ff821f7a8850
bansalnitesh <55881929+bansalnitesh@users.noreply.github.com>
2020-05-31 04:00:37 -0400

    api side

---
+++ README.md
@@ -0,0 +1,7 @@
+nitesh-bansal.com
+
+A website created using react and express on back end. Uses nodemailer to send out the form .
+
+The design is inspired from brittanychiang.com
+
+Feel free to download the code for personal use.
---
+++ server.js
@@ -0,0 +1,72 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const exphbs = require('express-handlebars');
+const path = require('path');
+const nodemailer = require('nodemailer');
+
+const app = express();
+
+// View engine setup
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
+// Static folder
+app.use('/public', express.static(path.join(__dirname, 'public')));
+
+// Body Parser Middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  res.render('contact');
+});
+
+app.post('/send', (req, res) => {
+  const output = `
+    <p>You have a new contact request</p>
+    <h3>Contact Details</h3>
+    <ul>  
+      <li>Name: ${req.body.name}</li>
+      <li>Subject: ${req.body.subject}</li>
+      <li>Email: ${req.body.email}</li>
+    </ul>
+    <h3>Message</h3>
+    <p>${req.body.message}</p>
+  `;
+
+  // create reusable transporter object using the default SMTP transport
+  let transporter = nodemailer.createTransport({
+    host: 'mail.nitesh-bansal.com',
+    port: 465,
+    secure: true, // true for 465, false for other ports
+    auth: {
+        user: 'yourcred', // generated ethereal user
+        pass: 'yourcred'  // generated ethereal password
+    },
+    tls:{
+      rejectUnauthorized:false
+    }
+  });
+
+  // setup email data with unicode symbols
+  let mailOptions = {
+      from: '"Nitesh Bansal" <admin@nitesh-bansal.com>', // sender address
+      to: 'niteshbansal30@gmail.com', // list of receivers
+      subject: 'Node Contact Request', // Subject line
+      text: 'Hello world?', // plain text body
+      html: output // html body
+  };
+
+  // send mail with defined transport object
+  transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+          return console.log(error);
+      }
+      console.log('Message sent: %s', info.messageId);
+      console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+
+      res.render('contact', {msg:'Email has been sent'});
+  });
+  });
+
+app.listen(5000, () => console.log('Server started...'));
